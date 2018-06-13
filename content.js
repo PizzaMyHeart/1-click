196e84878a177625897a32e9ac3f096bc48b99a4
PizzaMyHeart <ian_tan@hotmail.com>
2018-06-13 21:40:41 +0100

    science direct

---
+++ content.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  alert("hello");
+  if (document.contains(document.querySelector('[title="Other institution login"]'))) {
+    document.querySelector('[title="Other institution login"]').click()
+  } 
+  else if (document.contains(document.querySelector('[title="View all institutions"]')) && !document.contains(document.querySelector('#sdBody > div.pageContent > div:nth-child(4) > div:nth-child(2) > p:nth-child(6) > a:nth-child(318)'))) {
+    document.querySelector('[title="View all institutions"]').click()
+  }
+  else if (document.contains(document.querySelector('#sdBody > div.pageContent > div:nth-child(4) > div:nth-child(2) > p:nth-child(6) > a:nth-child(318)'))) {
+    document.querySelector('#sdBody > div.pageContent > div:nth-child(4) > div:nth-child(2) > p:nth-child(6) > a:nth-child(318)').click()
+  }
+  else if (document.contains(document.querySelector(".brand_heading"))) {
+    document.getElementByName("_eventId_proceed").click();
+  }
+}
